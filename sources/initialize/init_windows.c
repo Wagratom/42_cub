28195eb0baecc12f32371a43312671c418c2991b
devhector <hectorhu17@gmail.com>
2022-12-31 14:55:21 -0300

    fix: writing
    Signed-off-by: devhector <hectorhu17@gmail.com>

--- sources/initialize/init_windows.c
+++ sources/initialize/init_windows.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_windows.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 10:04:05 by wwallas-          #+#    #+#             */
-/*   Updated: 2022/12/05 13:15:18 by wwallas-         ###   ########.fr       */
+/*   Updated: 2022/12/31 10:39:24 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static t_bool	init_windows(t_data *data)
 	data->win = mlx_new_window(data->mlx, 1000, 1000, "Cub3D");
 	if (data->win == NULL)
 	{
-		printf("Erro creat windows"); //die
+		printf("Erro create windows"); //die
 		return (FALSE);
 	}
 	return (TRUE);
@@ -27,7 +27,7 @@ t_bool	init_windows_or_die(t_data *data)
 {
 	if (data->mlx == NULL)
 	{
-		printf("Not creat windows, mlx pointer null"); //die
+		printf("Not create windows, mlx pointer null"); //die
 		return (FALSE);
 	}
 	return (init_windows(data));
