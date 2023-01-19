ea70271a284467429e814e0b7a37eef7c677c11c
devhector <hectorhu17@gmail.com>
2023-01-19 13:43:06 -0300

    feat: gambi
    Signed-off-by: devhector <hectorhu17@gmail.com>

--- sources/program/filter_keyboard.c
+++ sources/program/filter_keyboard.c
@@ -6,22 +6,18 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 18:34:25 by wwallas-          #+#    #+#             */
-/*   Updated: 2023/01/06 16:26:42 by hectfern         ###   ########.fr       */
+/*   Updated: 2023/01/19 13:38:41 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 
-int	check_position(t_map *map, long x, long y)
+int	check_position(t_map *map, double x, double y)
 {
-	int	x_;
-	int	y_;
-
-	x_ = (int)x;
-	y_ = (int)y;
-	if (map->map[y_][x_] == '1')
+	printf("x = %f | y = %f | map: %c\n", x, y, map->map[(int)x][(int)y]);
+	if (map->map[(int)x][(int)y] == '1')
 		return (TRUE);
-	return (TRUE);
+	return (FALSE);
 }
 
 int	move_up(t_data *data)
@@ -32,6 +28,9 @@ int	move_up(t_data *data)
 	if (check_position(map, data->player.pos.x + \
 		data->player.dir.x * data->player.speed, data->player.pos.y))
 		data->player.pos.x += data->player.dir.x * data->player.speed;
+	if (check_position(map, data->player.pos.x, data->player.pos.y + \
+		data->player.dir.y * data->player.speed))
+		data->player.pos.y += data->player.dir.y * data->player.speed;
 	return (0);
 }
 
@@ -43,6 +42,9 @@ int	move_down(t_data *data)
 	if (check_position(map, data->player.pos.x - \
 		data->player.dir.x * data->player.speed, data->player.pos.y))
 		data->player.pos.x -= data->player.dir.x * data->player.speed;
+	if (check_position(map, data->player.pos.x, data->player.pos.y - \
+		data->player.dir.y * data->player.speed))
+		data->player.pos.y -= data->player.dir.y * data->player.speed;
 	return (0);
 }
 
@@ -52,15 +54,19 @@ int look_left(t_data *data)
 	double	old_plane_x;
 
 	old_dir_x = data->player.dir.x;
+	printf("1 - dir.x = %f | dir.y = %f\n", data->player.dir.x, data->player.dir.y);
 	data->player.dir.x = data->player.dir.x * cos(data->player.rot_speed) - \
 		data->player.dir.y * sin(data->player.rot_speed);
 	data->player.dir.y = old_dir_x * sin(data->player.rot_speed) + \
 		data->player.dir.y * cos(data->player.rot_speed);
+	printf("2 - dir.x = %f | dir.y = %f\n", data->player.dir.x, data->player.dir.y);
 	old_plane_x = data->player.plane.x;
 	data->player.plane.x = data->player.plane.x * cos(data->player.rot_speed) \
 		- data->player.plane.y * sin(data->player.rot_speed);
 	data->player.plane.y = old_plane_x * sin(data->player.rot_speed) + \
 		data->player.plane.y * cos(data->player.rot_speed);
+	printf("3 - plane.x = %f | plane.y = %f\n", data->player.plane.x, data->player.plane.y);
+	printf("pos.x = %f | pos.y = %f\n", data->player.pos.x, data->player.pos.y);
 	return (0);
 }
 
@@ -71,16 +77,20 @@ int	look_right(t_data *data)
 	t_player	*player;
 
 	player = &data->player;
+	printf("1 - dir.x = %f | dir.y = %f\n", data->player.dir.x, data->player.dir.y);
 	old_dir_x = player->dir.x;
 	player->dir.x = player->dir.x * cos(-player->rot_speed) - \
 		player->dir.y * sin(-player->rot_speed);
 	player->dir.y = old_dir_x * sin(-player->rot_speed) + \
 		player->dir.y * cos(-player->rot_speed);
 	old_plane_x = player->plane.x;
+	printf("2 - dir.x = %f | dir.y = %f\n", data->player.dir.x, data->player.dir.y);
 	player->plane.x = player->plane.x * cos(-player->rot_speed) - \
 		player->plane.y * sin(-player->rot_speed);
 	player->plane.y = old_plane_x * sin(-player->rot_speed) + \
 		player->plane.y * cos(-player->rot_speed);
+	printf("3 - plane.x = %f | plane.y = %f\n", data->player.plane.x, data->player.plane.y);
+	printf("pos.x = %f | pos.y = %f\n", data->player.pos.x, data->player.pos.y);
 	return (0);
 }
 
