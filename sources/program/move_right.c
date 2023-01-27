288833059cc9d36a908abd8172f167429a6d53fd
devhector <hectorhu17@gmail.com>
2023-01-26 21:03:47 -0300

    ♻ refactor
    Signed-off-by: devhector <hectorhu17@gmail.com>

--- sources/program/move_right.c
+++ sources/program/move_right.c
@@ -6,18 +6,32 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 18:34:25 by wwallas-          #+#    #+#             */
-/*   Updated: 2023/01/26 19:27:48 by hectfern         ###   ########.fr       */
+/*   Updated: 2023/01/26 20:57:54 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 
+static void	check_side_x(t_map *map, double position_y, double position_x)
+{
+	double	next_x;
+
+	next_x = position_x + map->dir[P_Y] * map->speed;
+	if (is_valid_position(map, next_x, map->player[P_Y]))
+		update_p_player(map, position_y, next_x);
+}
+
+static void	check_side_y(t_map *map, double position_y, double position_x)
+{
+	double	next_y;
+
+	next_y = position_y - map->dir[P_X] * map->speed;
+	if (is_valid_position(map, map->player[P_X], next_y))
+		update_p_player(map, next_y, position_x);
+}
+
 void	map_move_right(t_map *map)
 {
-	if (map->map[(int)map->player[P_Y]]
-	[(int)(map->player[P_X] + map->dir[P_Y] * map->speed)] == '0')
-		map->player[P_X] += map->dir[P_Y] * map->speed;
-	if (map->map[(int)(map->player[P_Y] - map->dir[P_X] * map->speed)]
-	[(int)(map->player[P_X])] == '0')
-		map->player[P_Y] -= map->dir[P_X] * map->speed;
+	check_side_x(map, map->player[P_Y], map->player[P_X]);
+	check_side_y(map, map->player[P_Y], map->player[P_X]);
 }
