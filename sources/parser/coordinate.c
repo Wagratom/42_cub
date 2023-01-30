444b0f9d6e2e4f38372572baa1bbb34edc1f6c51
wallas <wagratom111@gmail.com>
2023-01-30 16:56:01 -0300

    is coordinites

--- sources/parser/coordinate.c
+++ sources/parser/coordinate.c
@@ -1,16 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   texture_nort.c                                     :+:      :+:    :+:   */
+/*   coordinate.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wwalas- <wwallas-@student.42sp.org.br>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 14:37:33 by wwalas-           #+#    #+#             */
-/*   Updated: 2023/01/30 15:02:37 by wwalas-          ###   ########.fr       */
+/*   Updated: 2023/01/30 16:35:44 by wwalas-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
+
+char	*get_coordinate(char *str)
+{
+	char	*end_word;
+	int		len_word;
+
+	end_word = ft_strchr(str, ' ');
+	len_word = end_word - str;
+	return (ft_substr(str, 0, len_word));
+}
 
 t_bool	is_texture_north(char *str)
 {
@@ -18,10 +28,11 @@ t_bool	is_texture_north(char *str)
 	t_bool	status;
 
 	status = TRUE;
-	coordinate = ft_substr(str, 0, 2);
-	if (!ft_str_eq(str, "NO"))
-		status = FALSE ;
+	coordinate = get_coordinate(str);
+	if (!ft_str_eq(coordinate, "NO"))
+		status = FALSE;
 	free(coordinate);
+	coordinate = NULL;
 	return (status);
 }
 
@@ -32,8 +43,8 @@ t_bool	is_texture_south(char *str)
 
 
 	status = TRUE;
-	coordinate = ft_substr(str, 0, 2);
-	if (!ft_str_eq(str, "SO"))
+	coordinate = get_coordinate(str);
+	if (!ft_str_eq(coordinate, "SO"))
 		status = FALSE ;
 	free(coordinate);
 	return (status);
@@ -45,8 +56,8 @@ t_bool	is_texture_west(char *str)
 	t_bool	status;
 
 	status = TRUE;
-	coordinate = ft_substr(str, 0, 2);
-	if (!ft_str_eq(str, "WE"))
+	coordinate = get_coordinate(str);
+	if (!ft_str_eq(coordinate, "WE"))
 		status = FALSE ;
 	free(coordinate);
 	return (status);
@@ -58,8 +69,8 @@ t_bool	is_texture_east(char *str)
 	t_bool	status;
 
 	status = TRUE;
-	coordinate = ft_substr(str, 0, 2);
-	if (!ft_str_eq(str, "EA"))
+	coordinate = get_coordinate(str);
+	if (!ft_str_eq(coordinate, "EA"))
 		status = FALSE ;
 	free(coordinate);
 	return (status);
