9f220303b6f5987ac1a20bfe1c8ec10dbd1118b6
devhector <hectorhu17@gmail.com>
2023-02-11 21:19:12 -0300

    chore: norme

--- sources/parser/alloc_texture.c
+++ sources/parser/alloc_texture.c
@@ -3,16 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   alloc_texture.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wwalas- <wwallas-@student.42sp.org.br>     +#+  +:+       +#+        */
+/*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:35:37 by wwallas-          #+#    #+#             */
-/*   Updated: 2023/02/11 14:14:01 by wwalas-          ###   ########.fr       */
+/*   Updated: 2023/02/11 18:32:39 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 
-t_bool	
 t_bool	alloc_texture(t_map *data)
 {
 	int	index;
@@ -21,27 +20,16 @@ t_bool	alloc_texture(t_map *data)
 	size = sizeof(int) * TILE_SIZE * TILE_SIZE;
 	data->texture = (int **)malloc(sizeof(int *) * 4);
 	if (!data->texture)
-		return (msg_and_error(NULL, "Error: Error in malloc pointer to texture"));
+		return (msg_and_error(NULL, "Error: malloc pointer to texture"));
 	index = -1;
 	while (++index < 4)
 	{
 		data->texture[index] = (int *)malloc(size);
 		if (!data->texture[index])
-			return (msg_and_error(NULL, "Error: Error in malloc pointer to texture"));
-		ft_bzero(data->texture[index], size); // nao sei c da o msm efeito // pode usar o ft_calloc tb da na mesma
+			return (msg_and_error(NULL, "Error: malloc pointer to texture"));
+		ft_bzero(data->texture[index], size);
 	}
 	return (TRUE);
-}
-
-static t_bool	clear_texture(t_map *data)
-{
-	int	index;
-
-	index = -1;
-	while(++index < 4)
-		free(data->texture[index]);
-	free(data->texture);
-	return (FALSE);
 }
 
 t_bool	create_texture(t_data *data)
@@ -49,16 +37,16 @@ t_bool	create_texture(t_data *data)
 	int	index;
 	t_map	*map;
 
-	debug_printc(has_flag(), NULL, "allocando a pointers to texture: OK");
+	debug_printc(has_flag(), NULL, "pointers to texture: OK");
 	map = &data->map;
 	if (!alloc_texture(map))
 		return (FALSE);
 	index = -1;
 	while (++index < 4)
 	{
-		if (load_texture(data, map->texture[index], data->parser.coordinates[index]))
+		if (load_texture(data, map->texture[index], \
+			data->parser.coordinates[index]))
 			continue;
-		clear_texture(map);
 		return (msg_and_error("Error: ", "Not load texture"));
 	}
 	debug_printc(has_flag(), NULL, "Texture: Ok\n");
--- sources/parser/create_texture.c
+++ sources/parser/create_texture.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   create_texture.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wwalas- <wwallas-@student.42sp.org.br>     +#+  +:+       +#+        */
+/*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:35:37 by wwallas-          #+#    #+#             */
-/*   Updated: 2023/02/11 14:12:50 by wwalas-          ###   ########.fr       */
+/*   Updated: 2023/02/11 17:10:24 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,57 +51,5 @@ t_bool	load_texture(t_data *data, int *texture, char *path)
 			texture[img.width * i + j] = img.addr[img.width * i + j];
 	}
 	mlx_destroy_image(data->mlx, img.img);
-	return (TRUE);
-}
-
-t_bool	alloc_texture(t_map *data)
-{
-	int	index;
-	int	size;
-
-	size = sizeof(int) * TILE_SIZE * TILE_SIZE;
-	data->texture = (int **)malloc(sizeof(int *) * 4);
-	if (!data->texture)
-		return (msg_and_error(NULL, "Error: Error in malloc pointer to texture"));
-	index = -1;
-	while (++index < 4)
-	{
-		data->texture[index] = (int *)malloc(size);
-		if (!data->texture[index])
-			return (msg_and_error(NULL, "Error: Error in malloc pointer to texture"));
-		ft_bzero(data->texture[index], size); // nao sei c da o msm efeito // pode usar o ft_calloc tb da na mesma
-	}
-	return (TRUE);
-}
-
-static t_bool	clear_texture(t_map *data)
-{
-	int	index;
-
-	index = -1;
-	while(++index < 4)
-		free(data->texture[index]);
-	free(data->texture);
-	return (FALSE);
-}
-
-t_bool	create_texture(t_data *data)
-{
-	int	index;
-	t_map	*map;
-
-	debug_printc(has_flag(), NULL, "allocando a pointers to texture: OK");
-	map = &data->map;
-	if (!alloc_texture(map))
-		return (FALSE);
-	index = -1;
-	while (++index < 4)
-	{
-		if (load_texture(data, map->texture[index], data->parser.coordinates[index]))
-			continue;
-		clear_texture(map);
-		return (msg_and_error("Error: ", "Not load texture"));
-	}
-	debug_printc(has_flag(), NULL, "Texture: Ok\n");
 	return (TRUE);
 }
